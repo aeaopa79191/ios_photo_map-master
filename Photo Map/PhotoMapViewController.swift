ce5aab6820c6ecb26ad15ab3c24506665a10ba1a
Ka Kin Chiu <aeaopa79191@gmail.com>
2016-03-17 20:50:31 -0700

    complete Milestone 3: Take a Photo

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -9,11 +9,10 @@
 import UIKit
 import MapKit
 
-class PhotoMapViewController: UIViewController {
+class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     
     @IBOutlet weak var mapView: MKMapView!
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +24,45 @@ class PhotoMapViewController: UIViewController {
             MKCoordinateSpanMake(0.1, 0.1))
         mapView.setRegion(sfRegion, animated: false)
     }
+    
+    
+    
+    
+    
+    @IBAction func onCamara(sender: AnyObject) {
+        //Step 1: Instantiate a UIImagePickerController
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.presentViewController(vc, animated: true, completion: nil)
+        
+        
+    }
+    
 
+    //Step 2: Implement the delegate
+    func imagePickerController(picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+            // Get the image captured by the UIImagePickerController
+            let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+            let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+            
+            // Do something with the images (based on your use case)
+            
+            // Dismiss UIImagePickerController to go back to your original view controller
+            dismissViewControllerAnimated(true, completion: {self.performSegueWithIdentifier("tagSegue", sender: nil)})
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
